0200c86a0e69c475118168408ebbf616bd48b2bd
gdodet-lab <g.dodet@ob-rugby.fr>
2026-09-15 11:41:48 +0200

    Corriger API clubs

--- api/clubs.js
+++ api/clubs.js
@@ -53,9 +53,8 @@ export default async function handler(req, res) {
             "Content-Type": "application/json",
             Prefer: "return=representation"
           },
-         body: JSON.stringify({
-  name: name.trim()
-})
+          body: JSON.stringify({
+            name: name.trim()
           })
         }
       );
